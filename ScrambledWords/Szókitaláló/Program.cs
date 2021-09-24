9ac946ec684106071dd3e382ab290a169dfac662
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2021-09-24 23:45:25 +0200

    Initial commit

---
+++ ScrambledWords/Szókitaláló/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Szókitaláló
+{
+  public class Program
+  {
+    static void Main()
+    {
+      List<string> dictionary = File.ReadAllLines("dictionary.txt").ToList();
+      List<string> scrambleds = File.ReadAllLines("scrambled-words.txt").ToList();
+      List<string> unscrambleds = new();
+      foreach (string scrambled in scrambleds)
+      {
+        string unscrambled = Unscramble(scrambled, dictionary);
+        Console.WriteLine($"{scrambled} -> {unscrambled}");
+        unscrambleds.Add(unscrambled);
+      }
+
+      Console.ReadKey();
+    }
+
+    static string Unscramble(string scrambled, List<string> dictionary)
+    {
+      var scrambledChars = GetCharsFromWord(scrambled);
+      var result = dictionary
+        .Where(x => x.Length == scrambled.Length)
+        .Where(x => Enumerable.SequenceEqual(scrambledChars, GetCharsFromWord(x))).ToList();
+
+      return result.FirstOrDefault();
+    }
+
+    static List<char> GetCharsFromWord(string word)
+    {
+      var result = word.ToCharArray().ToList();
+      result.Sort();
+      return result;
+    }
+  }
+}
