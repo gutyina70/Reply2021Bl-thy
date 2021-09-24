3b559ad807a56768fd24ebaff6c7d83ff471c692
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2021-09-25 00:14:23 +0200

    Solve 'ScrambledWords', add c# gitignore

--- ScrambledWords/Szókitaláló/Program.cs
+++ ScrambledWords/Szókitaláló/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Szókitaláló
 {
@@ -11,7 +13,7 @@ namespace Szókitaláló
     {
       List<string> dictionary = File.ReadAllLines("dictionary.txt").ToList();
       List<string> scrambleds = File.ReadAllLines("scrambled-words.txt").ToList();
-      List<string> unscrambleds = new();
+      List<string> unscrambleds = new List<string>();
       foreach (string scrambled in scrambleds)
       {
         string unscrambled = Unscramble(scrambled, dictionary);
@@ -19,7 +21,25 @@ namespace Szókitaláló
         unscrambleds.Add(unscrambled);
       }
 
+      string unscrambledConcat = string.Join("", unscrambleds).ToLower();
+      File.WriteAllText("unscrambled.txt", unscrambledConcat);
+      string hash = Sha256(unscrambledConcat);
+      Console.WriteLine($"Hash: {hash}");
+      File.WriteAllText("flag.txt", $"{{FLG:{hash}}}");
+
       Console.ReadKey();
+    }
+
+    static string Sha256(string randomString)
+    {
+      var crypt = new SHA256Managed();
+      string hash = String.Empty;
+      byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+      foreach (byte theByte in crypto)
+      {
+        hash += theByte.ToString("x2");
+      }
+      return hash;
     }
 
     static string Unscramble(string scrambled, List<string> dictionary)
